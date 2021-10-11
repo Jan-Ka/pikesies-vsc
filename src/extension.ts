55298ca1946c2ea3af243d8b042eeda0c5d89a23
Jan-Ka <jan-ka@users.noreply.github.com>
2021-10-11 15:07:35 +0000

    changing command name" -m "just experimenting

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,12 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+// @ts-ignore 6059
+import { version } from '../package.json';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "pikesies" is now active!');
+	console.log(`Running pikesies ${version}`);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('pikesies.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
+	let disposable = vscode.commands.registerCommand('pikesies.validate', () => {
 		vscode.window.showInformationMessage('Hello World from VSC!');
 	});
 
